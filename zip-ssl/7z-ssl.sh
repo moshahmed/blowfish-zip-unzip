9173cc933f9c5989abf1022cf1953682450ac870
mosh <moshahmed@gmail.com>
2017-10-27 08:09:18 +0530

    use pemfile

--- zip-ssl/7z-ssl.sh
+++ zip-ssl/7z-ssl.sh
@@ -1,150 +1,92 @@
 #!/usr/bin/bash
 # What: 7z using public key for win7
-# $Header: c:/cvs/repo/mosh/perl/7z-ssl.sh,v 1.48 2017-10-14 11:05:39 a Exp $
+# $Header: c:/cvs/repo/mosh/perl/7z-ssl.sh,v 1.57 2017-10-27 02:38:37 a Exp $
 # GPL(C) moshahmed/at/gmail
 
-function die() { 1>&2 echo "$*" ; exit ;}
-function warn() { 1>&2 echo "$*" ;}
+function die() { 1>&2 echo -e "$*" ; exit ;}
+function warn() { 1>&2 echo -e "$*" ;}
 function info() { if [[ -n "$verbose" ]]; then 1>&2 echo "$*" ;fi ;}
-function need_file(){ test -f "$1" || die "need_file $1" ;}
-function need_dir(){ test -d "$1" || die "need_dir $1" ;}
+function need_file(){ test -f "$1" || die "need_file $*" ;}
+function need_dir(){ test -d "$1" || die "need_dir $*" ;}
 
 CMD=${0##*\\}
+
+function usage() { 1>&2 echo "
+What: $CMD [Options] [actions] [archive] .. 7z encrypt with openssl id_rsa
+  From: https://travis-ci.org/okigan/e7z
+    and https://wiki.openssl.org/index.php/Command_Line_Utilities
+Actions:
+  -a file.7z files         .. archive files into file.7z
+  -x file.7z               .. extract files from file.7z
+Options:
+  -key keyfile   .. keyfile, e.g. ~/.ssh/.id_rsa
+  -pem pemfile   .. keyfile.pem.pub, 
+    pemfile made with, ssh-keygen -f keyfile -e -m PKCS8 > pemfile
+  -v=1           .. verbose
+Example Usage:
+  $CMD -pem id_rsa.pem.pub -a archive.7z *.txt    # pack
+  $CMD -key id_rsa         -x archive.7z          # unpack
+"
+  echo "$*"
+  exit
+}
+
 zipper=7z
 
-export TMPDIR="$(mktemp -d)"
+export TMPDIR=$TMP
+need_dir $TMPDIR
+
 log=$TMPDIR/run.log
-keyfile=$TMPDIR/id_rsa.tmp
-otpfile=$TMPDIR/otp.ssl # encrypted otp with keyfile
+keyfile=$HOME/.ssh/id_rsa
+pemfile=$HOME/.ssh/id_rsa.pem.pub
+otpfile=otp.ssl # encrypted otp with keyfile
 archive=$TMPDIR/test.7z
 verbose=
 action=
 args=
 
-function usage() { 1>&2 echo "
-What: $CMD [Options] [actions] [7z options] .. 7z encrypt with openssl id_rsa
-  From: https://travis-ci.org/okigan/e7z
-  See https://wiki.openssl.org/index.php/Command_Line_Utilities
-Options:
-  -k keyfile   .. keyfile=~/.ssh/.id_rsa (default $keyfile)
-  -lcd dir     .. chdir dir
-  -v=1         .. verbose
-Actions:
-  -a           .. archive
-  -l           .. list archive
-  -x           .. extract
-Actions-setup:
-  -K keyfile   .. make keyfile using ssh-keygen
-  -o otpfile   .. extract otp from otpfile using keyfile
-  -t           .. test
-Where:
-  TMPDIR=mktemp -d, zipper=$zipper
-Example Usage:
-  $CMD -K id_rsa                        # first time, create id_rsa keypair
-  $CMD -k id_rsa -a archive.7z *.txt    # pack
-  $CMD           -l archive.7z          # list
-  $CMD -k id_rsa -x archive.7z          # unpack
-"
-  echo $*
-  exit
-}
-
-function testme() {
-  need_dir $TMPDIR
-  cd $TMPDIR
-  if [[ ! -e "$keyfile" ]] ;then
-    ssh-keygen -t rsa -f $keyfile -q -N ""
-    info "# Generated openssl rsa keyfile=$keyfile"
-  fi
-  need_file $keyfile
-  date > date.txt
-  bash $0 -v=$verbose -k $keyfile -a $archive date.txt # >> $log 2>&1
-  need_file $archive
-  bash $0 -v=$verbose -k $keyfile -x $archive -oout # >> $log 2>&1
-  need_file out/date.txt
-  diff   $PWD/date.txt $PWD/out/date.txt ; error=$?
-  if [ $error -eq 0 ] ; then
-    warn "test passed"
-  else
-    warn "test failed"
-  fi
-}
-
-function make_ssh_key() {
-  local keyfile=$1
-  warn "Creating keypair keyfile=$keyfile with ssh-keygen"
-  ssh-keygen -t rsa -f $keyfile -q
-  need_file $keyfile
-  need_file $keyfile.pub
-}
-
 # Options
 while [ $# -gt 0 ]  ;do
   case $1 in
-    -lcd) dir=${2:Need-lcd-dir} ; shift; need_dir $dir ; cd $dir ;;
-    -k) keyfile=${2:?Need-keyfile}; shift ;;
-    -K) keyfile=${2:?}; shift;
-        make_ssh_key $keyfile
-        exit ;;
-    -v=*) verbose=${1#-*=} ; info verbose=$verbose ;;
+    -key) keyfile=${2:?}; shift ;;
+    -pem) pemfile=${2:?}; shift ;;
     -v) verbose=1 ;;
+    -v=*) verbose=${1#-*=} ;;
     # break after actions, remaining args for 7z
-    -a) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -o) action=$1 ; otpfile=${2:?} ; shift 2; args=$* ; break ;;
-    -x) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -l) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -t) shift; testme ; exit ;;
+    -a) action=$1 ; archive=${2:?"Need archive.7z"} ; shift 2; args=$* ; break ;;
+    -x) action=$1 ; archive=${2:?"Need archive.7z"} ; shift 2; args=$* ; break ;;
     *) usage "Unknown option:'$*'" ;;
   esac
   shift
 done
 
 info "=== action=$action, archive=$archive, keyfile=$keyfile, args=$args"
+
 case $action in
-  -o) warn otp=$(cat $otpfile | openssl pkeyutl -decrypt -inkey $keyfile) ; exit ;;
   -a)
-    # generate otp (one time password)
+    # Generate otp (one time password)
     otp=$(openssl rand -hex 32|dos2unix)
-    # Save encrypted otp = ssl_enc(keyfile,otp) in the archive, -si from stdin.
-    echo $otp | openssl pkeyutl -encrypt -inkey $keyfile -out $otpfile
+    info "# Generated otp=$otp"
+    need_file $pemfile "\nDo: ssh-keygen -f $keyfile -e -m PKCS8 > $pemfile"
+    echo $otp |
+      openssl pkeyutl -encrypt -pubin -inkey $pemfile -out $otpfile
     need_file $otpfile
-    info "# Generated otp=$otp "
-    info "# Encryped opt with $keyfile to otpfile=$otpfile"
-    # save otpfile unencrypted
-    # cat $otpfile | $zipper a $archive -si$otpfile
-    info "$zipper a        $archive $otpfile"
-    info "$zipper u -p$otp $archive $args"
-    $zipper a        $archive $otpfile  >> $log 2>&1
-    $zipper u -p$otp $archive $args  >> $log 2>&1
+    info "# Encryped opt with $pemfile to otpfile=$otpfile"
+    # Save encrypted otp = otpfile = ssl_enc(keyfile,otp) in the archive, -si from stdin.
+    info "# Encrypting $archive with otpfile=$otpfile=$otp"
+    cat $otpfile | $zipper a        $archive -si$otpfile
+                   $zipper u -p$otp $archive    $args
     need_file $archive
-    info "# Encrypted $archive with otpfile=$otpfile=$otp"
-    # Extract otp from archive using ssl keyfile, -so to stdout.
-    if [[ -n "$verbose" ]] ;then
-      $zipper l $archive >> $log 2>&1
-    fi
-    otp2=`$zipper x -so $archive ${otpfile//*\/} | openssl pkeyutl -decrypt -inkey $keyfile`
-    # check top can be extracted correctly from archive with ssl using keyfile
-    if [[ "$otp2" != "$otp" ]] ;then
-      die "# opt extraction error otp=$otp!=$otp2 in $archive in $otpfile"
-    else
-      info "# opt extracted correctly"
-    fi
-    ;;
-  -l) # list archive
-    warn "$zipper l $archive"
-    $zipper l $archive -p$otp $args -x!$otpfile | perl -lne 'print if m/^---/.../^---/'
     ;;
   -x)
-    # Extract otp from archive using ssl keyfile, -so to stdout.
-    otp=`$zipper x -so $archive ${otpfile//*\/} | openssl pkeyutl -decrypt -inkey $keyfile 2>> $log`
+    need_file $archive
+    # Extract otp from archive using keyfile -so to stdout.
+    otp=$($zipper x -so $archive $otpfile | openssl pkeyutl -decrypt -inkey $keyfile )
     if [[ -z "$otp" ]] ;then
-      die "No otp=$otp in $archive in $otpfile"
+      die "No otp=$otp in $archive/$otpfile"
     fi
-    # use otp to extract archive.
-    otpbase=${otpfile//*\/}
-    warn "# $zipper x $archive -p$otp $args -x!$otpbase"
-            $zipper x $archive -p$otp $args -x!$otpbase >> $log 2>&1
-    info "# Decrypted $archive with otp=$otp"
+    info "# Decrypting $archive with otp=$otp"
+    $zipper x $archive -p$otp $args -x!$otpfile
     ;;
   *) usage "Nothing to do '$action'?" ;;
 esac
--- zip-ssl/zip-ssl.sh
+++ zip-ssl/zip-ssl.sh
@@ -1,155 +1,93 @@
 #!/usr/bin/bash
 # What: zip using public key for win7
-# $Header: c:/cvs/repo/mosh/perl/zip-ssl.sh,v 1.28 2017-10-14 11:05:40 a Exp $
+# $Header: c:/cvs/repo/mosh/perl/zip-ssl.sh,v 1.30 2017-10-27 02:38:37 a Exp $
 # GPL(C) moshahmed/at/gmail
 
-function die() {  1>&2 echo "$*" ; exit ;}
-function warn() { 1>&2 echo "$*" ;}
+function die() { 1>&2 echo -e "$*" ; exit ;}
+function warn() { 1>&2 echo -e "$*" ;}
 function info() { if [[ -n "$verbose" ]]; then 1>&2 echo "$*" ;fi ;}
-function need_file(){ test -f "$1" || die "need_file $1" ;}
-function need_dir(){ test -d "$1" || die "need_dir $1" ;}
+function need_file(){ test -f "$1" || die "need_file $*" ;}
+function need_dir(){ test -d "$1" || die "need_dir $*" ;}
 
 CMD=${0##*\\}
+
+function usage() { 1>&2 echo "
+What: $CMD [Options] [actions] [archive] .. zip encrypt with openssl id_rsa
+  See https://wiki.openssl.org/index.php/Command_Line_Utilities
+    and https://wiki.openssl.org/index.php/Command_Line_Utilities
+Actions:
+  -a file.zip files         .. archive files into file.zip
+  -x file.zip               .. extract files from file.zip
+Options:
+  -key keyfile   .. keyfile, e.g. ~/.ssh/.id_rsa
+  -pem pemfile   .. keyfile.pem.pub, 
+    pemfile made with, ssh-keygen -f keyfile -e -m PKCS8 > pemfile
+  -v=1           .. verbose
+Example Usage:
+  $CMD -pem id_rsa.pem.pub -a archive.zip *.txt    # pack
+  $CMD -key id_rsa         -x archive.zip          # unpack
+"
+  echo "$*"
+  exit
+}
+
 zipper=zip
 unzipper=unzip
 
-export TMPDIR="$(mktemp -d)"
-log=$TMPDIR/run.log
+export TMPDIR=$TMP
+need_dir $TMPDIR
 
-keyfile=$TMPDIR/id_rsa.tmp
-otpfile_base=otp.ssl
-otpfile=$TMPDIR/$otpfile_base # encrypted otp with keyfile
+log=$TMPDIR/run.log
+keyfile=$HOME/.ssh/id_rsa
+pemfile=$HOME/.ssh/id_rsa.pem.pub
+otpfile=otp.ssl # encrypted otp with keyfile
 archive=$TMPDIR/test.zip
 verbose=
 action=
 args=
 
-
-function usage() { 1>&2 echo "
-What: $CMD [Options] [actions] [zip options] .. zip encrypt with openssl id_rsa
-  See https://wiki.openssl.org/index.php/Command_Line_Utilities
-Options:
-  -k keyfile   .. keyfile=~/.ssh/.id_rsa (default $keyfile)
-  -lcd dir     .. chdir dir
-  -v=1         .. verbose
-Actions:
-  -a           .. archive
-  -l           .. list archive
-  -x           .. extract
-Actions-setup:
-  -K keyfile   .. make keyfile using ssh-keygen
-  -o otpfile   .. extract otp from otpfile using keyfile
-  -t           .. test
-Where:
-  TMPDIR=mktemp -d, zipper=$zipper, unzipper=$unzipper
-Example Usage:
-  $CMD -K id_rsa                             # first time, create id_rsa keypair
-  $CMD -k id_rsa -a archive.zip *.txt        # pack
-  $CMD           -l archive.zip               # list
-  $CMD -k id_rsa -x archive.zip              # unpack
-"
-  echo $*
-  exit
-}
-
-function testme() {
-  need_dir $TMPDIR
-  cd $TMPDIR
-  if [[ ! -e "$keyfile" ]] ;then
-    ssh-keygen -t rsa -f $keyfile -q -N ""
-    info "# Generated openssl rsa keyfile=$keyfile"
-  fi
-  need_file $keyfile
-  date > date.txt
-  info "# bash $0 -v=$verbose -k $keyfile -a $archive date.txt"
-          bash $0 -v=$verbose -k $keyfile -a $archive date.txt
-  need_file $archive
-  info "# bash $0 -v=$verbose -k $keyfile -x $archive -d out"
-          bash $0 -v=$verbose -k $keyfile -x $archive -d out
-  need_file out/date.txt
-  diff   $PWD/date.txt $PWD/out/date.txt ; error=$?
-  if [ $error -eq 0 ] ; then
-    warn "test passed"
-  else
-    warn "test failed"
-  fi
-}
-
-function make_ssh_key() {
-  local keyfile=$1
-  warn "Creating keypair keyfile=$keyfile with ssh-keygen"
-  ssh-keygen -t rsa -f $keyfile -q
-  need_file $keyfile
-  need_file $keyfile.pub
-}
-
 # Options
 while [ $# -gt 0 ]  ;do
   case $1 in
-    -lcd) dir=${2:Need-lcd-dir} ; shift; need_dir $dir ; cd $dir ;;
-    -k) keyfile=${2:?Need-keyfile}; shift ;;
-    -K) keyfile=${2:?}; shift;
-        make_ssh_key $keyfile
-        exit ;;
+    -key) keyfile=${2:?}; shift ;;
+    -pem) pemfile=${2:?}; shift ;;
     -v) verbose=1 ;;
-    -v=*) verbose=${1#-*=} ; info verbose=$verbose ;;
+    -v=*) verbose=${1#-*=} ;;
     # break after actions, remaining args for zip
-    -a) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -o) action=$1 ; otpfile=${2:?} ; shift 2; args=$* ; break ;;
-    -x) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -l) action=$1 ; archive=${2:?} ; shift 2; args=$* ; break ;;
-    -t) shift ; testme ; exit ;;
+    -a) action=$1 ; archive=${2:?"Need archive.zip"} ; shift 2; args=$* ; break ;;
+    -x) action=$1 ; archive=${2:?"Need archive.zip"} ; shift 2; args=$* ; break ;;
     *) usage "Unknown option:'$*'" ;;
   esac
   shift
 done
 
 info "=== action=$action, archive=$archive, keyfile=$keyfile, args=$args"
+
 case $action in
-  -o) warn otp=$(cat $otpfile | openssl pkeyutl -decrypt -inkey $keyfile) ; exit ;;
   -a)
-    # generate otp (one time password)
+    # Generate otp (one time password)
     otp=$(openssl rand -hex 32|dos2unix)
-    # Save encrypted otp = ssl_enc(keyfile,otp) in the archive, -si from stdin.
-    echo $otp | openssl pkeyutl -encrypt -inkey $keyfile -out $otpfile
-    # cat otpfile | $zipper a $archive -si$otpfile
+    info "# Generated otp=$otp"
+    need_file $pemfile "\nDo: ssh-keygen -f $keyfile -e -m PKCS8 > $pemfile"
+    echo $otp |
+      openssl pkeyutl -encrypt -pubin -inkey $pemfile -out $otpfile
     need_file $otpfile
-    info "# Generated otp=$otp "
-    info "# Encryped opt with $keyfile to otpfile=$otpfile"
-    # save otpfile unencrypted
-    warn "# zip $archive"
+    info "# Encryped opt with $pemfile to otpfile=$otpfile"
+    # Save encrypted otp = otpfile = ssl_enc(keyfile,otp) in the archive
+    info "# Encrypting $archive with otpfile=$otpfile=$otp"
     $zipper -j           $archive $otpfile
     $zipper -u -P "$otp" $archive $args
     need_file $archive
-    info "# Archived $archive encrypted with otp in otpfile=$otpfile"
-    info "# otp=$otp"
-    # Extract otp from archive using ssl keyfile, -so to stdout.
-    if [[ -n "$verbose" ]] ;then
-      $unzipper -lv $archive | perl -lne 'print if m/^---/.../^---/'
-    fi
-    otp2=`$unzipper -p $archive "*$otpfile_base" | openssl pkeyutl -decrypt -inkey $keyfile`
-    if [[ "$otp2" != "$otp" ]] ;then
-      die "# opt extraction error otp=$otp!=$otp2 in $archive in $otpfile"
-    else
-      info "# opt extracted correctly"
-    fi
-    ;;
-  -l) # list archive
-    warn "$zipper -lv $archive"
-    $unzipper -vl $archive $args | perl -lne 'print if m/^---/.../^---/'
     ;;
   -x)
-    # Extract otp from archive using ssl keyfile, -so to stdout.
-    otp=`$unzipper -p $archive "*$otpfile_base" | openssl pkeyutl -decrypt -inkey $keyfile`
+    need_file $archive
+    # Extract otp from archive using keyfile
+    otp=$($zipper x -so $archive $otpfile | openssl pkeyutl -decrypt -inkey $keyfile )
     if [[ -z "$otp" ]] ;then
-      die "No otp=$otp in $archive in $otpfile"
+      die "No otp=$otp in $archive/$otpfile"
     fi
-    # use otp to extract archive.
-    warn "# $unzipper -P        $archive $args -x $otpfile_base"
-            $unzipper -P "$otp" $archive $args -x $otpfile_base
-    info "# Decrypted $archive with"
-    info "# otp=$otp"
+    info "# Decrypting $archive with otp=$otp"
+    $unzipper -P "$otp" $archive $args -x $otpfile_base
     ;;
   *) usage "Nothing to do '$action'?" ;;
 esac
-
