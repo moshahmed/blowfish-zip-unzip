82a58e5069cba5daebfed0cca5b9fe0b8600e56e
mosh <moshahmed@gmail.com>
2021-03-16 17:21:37 +0530

    print name

--- python/py-otp.py
+++ python/py-otp.py
@@ -5,27 +5,31 @@ Date:  2021-03-12 moshahmed/at/gmail
 Setup:
   Install python3 and pip install pyotp cryptography
   See https://pyotp.readthedocs.io/en/latest/
-Example:
+Example usage:
   $ echo 'my/aws1/comment:JBSWY3DPEHPK3PXP' | python py-otp.py aws1
     332556 aws1
-OR
+OR from bash and gpg
   echo '
     aws1:JBSW-Y3DP-EHPK-3PXP
     aws2:ZZXW6-33PN5X-W6MZX
-  ' | gpg -o  demo.gpg -qc
+    QR-Code:otpauth://totp/aws3?secret=JBSWY3DPEHPK3PXP&issuer=self
+  ' > demo.txt
+  $ gpg -o  demo.gpg -qc demo.txt
   $ gpg -qd demo.gpg | python py-otp.py aws1
     pass:fkey
     332556 aws1
-OR from inside vim
-  > vi -x seeds.txt
+OR from vim
+  $ gvim -x demo.txt
       pass:fkey
-      <CURSOR_ON_LINE> aws1/comment:JBSW-Y3DP-EHPK-3PXP
+      <CURSOR_ON_LINE> aws1:JBSW-Y3DP-EHPK-3PXP
       : command MoshGauthOtp : . w ! python py-otp.py ''
       : MoshGauthOtp
-      : . w ! python py-otp.py aws1
-      OR <visual-select SEED>
-      : '<,'> w ! python py-otp.py
-      vi show otp 332556
+        : . w ! python py-otp.py aws1
+          OR <visual-select SEED>
+        : '<,'> w ! python py-otp.py aws1
+          332556 aws1
+      :q
+  $
 '''
 
 import pyotp
@@ -35,27 +39,29 @@ def get_pyotp(line, pattern=''):
   # Input line is NAME:SEED and PATTERN =~ LINE
   if pattern and not re.search(pattern,line):
     return
-  qrcode = re.search(r'secret=(\w+)',line)
+  qrcode = re.search(r'totp/(\w+).*secret=(\w+)',line)
   if qrcode:
-    line = qrcode.group(1)
-  else:
-    line = re.sub(r'^.*:', '', line)
-  line = re.sub(r'\W+', '', line)
-  if not line:
-    return
+    name = qrcode.group(1)
+    seed = qrcode.group(2)
+  elif re.match(r'^.+:.+$',line):
+    name, seed = line.rsplit(':', 1)
+
+  seed = re.sub(r'\W+', '', seed)
+  if not seed:
+    return None, None
   try:
-    otp = pyotp.TOTP(line).now()
-    return otp
+    otp = pyotp.TOTP(seed).now()
+    return otp, name
   except:
-    return
+    return None, None
 
 def demo_pyotp():
   line ="aws1/demo:JBSW-Y3DP-EHPK-3PXP"
   pattern = 'demo'
-  otp = get_pyotp(line, pattern)
+  otp, name = get_pyotp(line, pattern)
   print("Example:")
   print(f"  $ echo '{line}' | python py-otp.py {pattern}")
-  print(f"    | otp={otp} {pattern}")
+  print(f"    | otp={otp} {name}")
 
 if __name__ == '__main__':
   if len(sys.argv) < 2:
@@ -65,7 +71,7 @@ if __name__ == '__main__':
 
   pattern = sys.argv[1]
   for line in sys.stdin:
-    otp = get_pyotp(line,pattern)
+    otp, name = get_pyotp(line,pattern)
     # print("DEBUG: %s %s %s" % (otp,pattern,line))
     if otp:
-      print("%s %s" % (otp,pattern))
+      print("  %s %s" % (otp,name))
